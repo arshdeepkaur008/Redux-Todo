c36109f355a8b6fb12adaa3b5fe1de2617dace7e
arshdeep kaur <arshdeep.kaur@almabay.com>
2024-04-29 11:15:20 +0530

    file added to new branch

--- components/Hey.js
+++ components/Hey.js
@@ -1,8 +1,11 @@
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Hey = () => {
     return (
+        <View>
         <Text>Helloo</Text>
+        <Text>This is a new branch, named as hello</Text>
+        </View>
     )
 } 
 export default Hey
